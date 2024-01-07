d34dc4df13ba3d16af707a9987ddacdb214d64b1
Natalia Kelim Thiel <natalia.kthiel@gmail.com>
2024-01-07 16:40:35 -0300

    feat: stores

--- src/presentation/screens/main/home.screen.tsx
+++ src/presentation/screens/main/home.screen.tsx
@@ -1,10 +1,19 @@
+import { useFocusEffect } from "@react-navigation/native";
 import React from "react";
 import { View, Text } from "react-native";
+import { useUserStore } from "stores/user.store";
 
 function HomeScreen() {
+  const firstName = useUserStore((state) => state.firstName);
+  const updateFirstName = useUserStore((state) => state.updateFirstName);
+
+  useFocusEffect(() => {
+    updateFirstName("John");
+  });
+
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Home Screen</Text>
+      <Text>Welcome {firstName}!</Text>
     </View>
   );
 }
