14ef5c9c2a4745b8e45fa76dc9c207a8ca104dc8
Natalia Kelim Thiel <natalia.kthiel@gmail.com>
2024-01-07 15:58:25 -0300

    feat: navigation and absolute path

--- babel.config.js
+++ babel.config.js
@@ -6,5 +6,30 @@ module.exports = function (api) {
       ["@babel/preset-env", { targets: { node: "current" } }],
       "@babel/preset-typescript",
     ],
+    plugins: [
+      [
+        "module-resolver",
+        {
+          root: ["./src"],
+          alias: {
+            // Application
+            hooks: "./src/application/hooks",
+            reducers: "./src/application/reducers",
+            stores: "./src/application/stores",
+            utils: "./src/application/utils",
+            // Domain
+            entities: "./src/domain/entities",
+            enums: "./src/domain/enums",
+            // Infrastructure
+            api: "./src/infrastructure/api",
+            // Presentation
+            assets: "./src/presentation/assets",
+            components: "./src/presentation/components",
+            navigation: "./src/presentation/navigation",
+            screens: "./src/presentation/screens",
+          },
+        },
+      ],
+    ],
   };
 };
---
+++ src/presentation/navigation/main.navigator.tsx
@@ -0,0 +1,18 @@
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import HomeScreen from "screens/main/home.screen";
+
+const Stack = createNativeStackNavigator();
+
+function MainNavigator() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default MainNavigator;
